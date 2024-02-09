747f0da7a00e95454ccd04ef668e681c27f13fc7
Sterling Wright <30376507+SterlingLWright@users.noreply.github.com>
2024-02-09 14:57:48 -0500

    Update Alignment_script.sh

--- SP24_09FEB24_Phylogenomics/scripts/Alignment_script.sh
+++ SP24_09FEB24_Phylogenomics/scripts/Alignment_script.sh
@@ -12,9 +12,6 @@ module load anaconda3
 # Change path to where you saved the Reference genome
 REF=/storage/group/LiberalArts/default/lsw132_collab/04_LabMembersFolders/SterlingLWright/DAWG_Phylogenomics/reference/Anaerolineacea_bacterium_oral_taxon_439.fasta
 
-# Change path to where you have the fastq files
-PATH=/gpfs/group/LiberalArts/default/lsw132_collab/04_LabMembersFolders/SterlingLWright
-
 # Index the reference
 bwa index $REF
 samtools faidx $REF
